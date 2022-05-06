821323c062e2e2d93f6c4a30111fa1631dbd312e
bernalde <bernalde@cmu.edu>
2022-05-06 03:55:18 -0400

    Rewritten random search and newer results

--- src/generate_ws_dataframe.py
+++ src/generate_ws_dataframe.py
@@ -11,6 +11,7 @@ import functools
 from typing import List, Tuple, Union
 from bisect import bisect_left
 from math import hypot
+import random
 
 idx = pd.IndexSlice
 
@@ -298,34 +299,44 @@ for size in sizes:
         df_stats = None
     else:
         df_stats = pd.read_pickle(df_path_stats)
-        df_stats_interpolated = interpolate_df(
-            dataframe=df_stats,
-            resource_column='reads',
-            prefix=df_name_stats,
-            parameters_dict=parameters_dict,
-            default_boots=default_boots,
-            minimum_boots=minimum_boots,
-            resource_proportional_parameters=['sweep', 'replica'],
-            idx = idx,
-            save_pickle=True,
-        )
+        if overwrite_pickles:
+            df_stats_interpolated = interpolate_df(
+                dataframe=df_stats,
+                resource_column='reads',
+                prefix=df_name_stats,
+                parameters_dict=parameters_dict,
+                default_boots=default_boots,
+                minimum_boots=minimum_boots,
+                resource_proportional_parameters=['sweep', 'replica'],
+                idx = idx,
+                save_pickle=True,
+            )
+        else:
+            df_name_interpolated = df_name_stats.rsplit('.')[0] + '_interp.pkl'
+            df_path_interpolated = os.path.join(results_path, df_name_interpolated)
+            df_stats_interpolated = pd.read_pickle(df_path_interpolated)
     df_name_results = prefix + 'df_results.pkl'
     df_path_results = os.path.join(results_path, df_name_results)
     if not os.path.exists(df_path_results):
         df_results = None
     else:
         df_results = pd.read_pickle(df_path_results)
-        df_results_interpolated = interpolate_df(
-            dataframe=df_results,
-            resource_column='reads',
-            prefix=df_name_results,
-            parameters_dict=parameters_dict,
-            default_boots=default_boots,
-            minimum_boots=minimum_boots,
-            resource_proportional_parameters=['sweep', 'replica'],
-            idx = idx,
-            save_pickle=True,
-        )
+        if overwrite_pickles:
+            df_results_interpolated = interpolate_df(
+                dataframe=df_results,
+                resource_column='reads',
+                prefix=df_name_results,
+                parameters_dict=parameters_dict,
+                default_boots=default_boots,
+                minimum_boots=minimum_boots,
+                resource_proportional_parameters=['sweep', 'replica'],
+                idx = idx,
+                save_pickle=True,
+            )
+        else:
+            df_name_interpolated = df_name_results.rsplit('.')[0] + '_interp.pkl'
+            df_path_interpolated = os.path.join(results_path, df_name_interpolated)
+            df_results_interpolated = pd.read_pickle(df_path_interpolated)
 
 # %%
 
@@ -333,11 +344,13 @@ for size in sizes:
 # TODO This can be generalized as using as groups the parameters that are not dependent of the metric (e.g., schedule) or that signify different solvers
 # TODO This needs to be functionalized
 stale_parameters = []
+varying_parameters = []
 numeric_parameters = []
 for parameter_name in parameter_names:
     if len(locals()[parameter_name+'s']) == 1:
         stale_parameters.append(parameter_name)
     else:
+        varying_parameters.append(parameter_name)
         if isinstance(locals()[parameter_name+'s'][0],int) or isinstance(locals()[parameter_name+'s'][0],float):
             numeric_parameters.append(parameter_name)
 
@@ -392,7 +405,8 @@ recipe_lazy = df_stats_interpolated[
     ].set_index(
         parameter_names
     ).groupby(['reads']
-        ).idxmax()
+        ).idxmax().rename(
+            columns={'median_'+response_column:'recipe'})
 
 df_virtual = df_virtual.merge(
     df_stats_interpolated[
@@ -401,6 +415,23 @@ df_virtual = df_virtual.merge(
             parameter_names
             ).groupby(['reads']
             ).max().reset_index().rename(columns={'median_perf_ratio':'lazy_perf_ratio'}))
+
+recipe_lazy = recipe_lazy.merge(
+    recipe_lazy['recipe'].apply(pd.Series).rename(
+        columns={i: parameter_names[i] for i in range(len(parameter_names))}
+        ).reset_index(),
+        on=['reads'],
+        how='left')
+
+recipe_lazy=recipe_lazy.merge(
+    df_stats_interpolated[
+        ['median_'+response_column] + parameter_names + ['reads']
+    ].set_index(
+            parameter_names
+            ).groupby(['reads']
+            ).max().reset_index(),
+        on=['reads'],
+        how='left')
 
 # Recipe to obtain parameter setting that optimizes the response  in the results dataframe for each instance and read, then takes the mean of the parameters across instances, for each read.
 # There is an extra projection step into the parameter values
@@ -435,7 +466,7 @@ df_results_interpolated['params'] = df_results_interpolated[parameter_names].app
 
 # Projecting parameter setting absent in the original dataframe to one that is available using the Euclidean norm.
 # TODO The euclidean norm does not take into account the fact that the scale of the parameters is different. Moreover, it does not work with non-numerical data
-# TODO performnace improvement here. This is by no means efficient.
+# TODO performance improvement here. This is by no means efficient.
 included_parameters = df_results_interpolated[numeric_parameters].values
 dist_eucl = lambda a, b: hypot(b[0]-a[0], b[0]-a[0])
 if not all([i in df_results_interpolated['params'].values for i in recipe_mean_best_params['params'].values]):
@@ -468,7 +499,288 @@ df_virtual = df_virtual.merge(
     on=['reads'],
     how='left')
 
+recipe_mean_best_params = recipe_mean_best_params.merge(
+    dummy_df.reset_index()[
+        ['median_'+response_column] + ['reads']
+    ],
+    on=['reads'],
+    how='left')
 
+df_name_virtual = df_name_results.rsplit('.')[0] + '_virtual.pkl'
+df_path_virtual = os.path.join(results_path, df_name_virtual)
+df_virtual.to_pickle(df_path_virtual)
+
+df_name_recipe_mean_best = df_name_results.rsplit('.')[0] + '_recipe_mean_best.pkl'
+df_path_recipe_mean_best = os.path.join(results_path, df_name_recipe_mean_best)
+recipe_mean_best_params.to_pickle(df_path_recipe_mean_best)
+
+df_name_recipe_mean_best = df_name_results.rsplit('.')[0] + '_recipe_mean_best.pkl'
+df_path_recipe_mean_best = os.path.join(results_path, df_name_recipe_mean_best)
+recipe_mean_best_params.to_pickle(df_path_recipe_mean_best)
+
+# %%
+# Function to perform alternative processing of progress dataframes
+
+
+def process_df_progress(
+    df_progress: pd.DataFrame = None,
+    compute_metrics: list = ['perf_ratio'],
+    stat_measures: list = ['median'],
+    maximizing: bool = True,
+) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    '''
+    Function to process progress dataframes, computing statistics across experiments and reporting best behavior for each budget.
+
+    Args:
+        df_progress: Dataframe containing progress data
+        compute_metrics: List of metrics to compute
+        stat_measures: List of statistics to compute
+        maximizing: Boolean indicating whether to maximize or minimize the metric
+
+    Returns:
+        Tuple of dictionaries of which there are
+            df_progress_processed: Processed dataframe
+            df_progress_end: Processed dataframe
+
+
+    '''
+    if df_progress is None:
+        return None
+
+    experiment_setting = ['R_budget', 'R_explor', 'run_per_solve']
+    individual_run = experiment_setting.copy()
+    if 'experiment' in df_progress.columns:
+        individual_run += ['experiment']
+
+    if maximizing:
+        opt_stats = ['max', 'idxmax']
+        del_str = 'max_'
+    else:
+        opt_stats = ['min', 'idxmin']
+        del_str = 'min_'
+
+    df_progress_end = df_progress[
+        individual_run + compute_metrics + ['cum_reads']
+    ].loc[
+        df_progress.sort_values(
+            ['cum_reads'],
+            ascending=False,
+        ).groupby(
+            individual_run
+        )['cum_reads'].idxmax()
+    ].copy()
+
+    for compute_metric in compute_metrics:
+        if 'inv_' in compute_metric:
+            df_progress[compute_metric.replace(
+                'inv_', '')] = 1 - df_progress[compute_metric] + EPSILON
+            expanding_metric = compute_metric
+            df_progress_end[compute_metric.replace(
+                'inv_', '')] = 1 - df_progress_end[compute_metric] + EPSILON
+        else:
+            position = compute_metric.index('perf_ratio')
+            df_progress[compute_metric[:position] + 'inv_' +
+                        compute_metric[position:]] = df_progress[compute_metric] + EPSILON
+            df_progress_end[compute_metric[:position] + 'inv_' +
+                            compute_metric[position:]] = df_progress_end[compute_metric] + EPSILON
+            expanding_metric = compute_metric[:position] + \
+                'inv_' + compute_metric[position:]
+    df_progress['best_inv_perf_ratio'] = df_progress.sort_values(
+        ['cum_reads', 'R_budget']
+    ).expanding(min_periods=1).min()[expanding_metric]
+    df_progress['best_perf_ratio'] = 1 - \
+        df_progress['best_inv_perf_ratio'] + EPSILON
+    
+    if 'f_explor' not in df_progress:
+        df_progress['f_explor'] = df_progress['R_explor'] / \
+            df_progress['R_budget']
+
+    # df_progress = cleanup_df(df_progress)
+
+    if 'f_explor' not in df_progress_end:
+        df_progress_end['f_explor'] = df_progress_end['R_explor'] / \
+            df_progress_end['R_budget']
+    # df_progress_end = cleanup_df(df_progress_end)
+
+    df_progress_best = df_progress_end.groupby(
+        experiment_setting
+    )[
+        compute_metrics
+    ].agg(stat_measures).groupby(
+        ['R_budget']
+    ).agg(
+        opt_stats
+    ).copy()
+
+    df_progress_best.columns = ["_".join(reversed(pair)).replace(
+        del_str, '') for pair in df_progress_best.columns]
+    df_progress_best.reset_index(inplace=True)
+
+    for stat_measure in stat_measures:
+        for compute_metric in compute_metrics:
+            if 'inv_' in compute_metric:
+                df_progress_best[stat_measure + '_' + compute_metric.replace('inv_', '')] = 1 - \
+                    df_progress_best[stat_measure +
+                                     '_' + compute_metric] + EPSILON
+            else:
+                position = compute_metric.index('perf_ratio')
+                df_progress_best[stat_measure + '_' + compute_metric[:position] + 'inv_' +
+                                 compute_metric[position:]] = 1 - df_progress_best[stat_measure + '_' + compute_metric] + EPSILON
+
+    # df_progress_best = cleanup_df(df_progress_best)
+
+    for column in df_progress_best.columns:
+        if 'idx' in column:
+            df_progress_best=df_progress_best.merge(df_progress_best[
+                'idxmean_median_perf_ratio'
+                ].apply(pd.Series).rename(
+                columns={i: experiment_setting[i] for i in range(len(experiment_setting))}
+                ), on='R_budget', how='left')
+
+    if 'f_explor' not in df_progress_best.columns:
+        df_progress_best['f_explor'] = df_progress_best['R_explor'] / \
+            df_progress_best['R_budget']
+    return df_progress_best, df_progress_end
+
+# %%
+# Random search for the ensemble
+repetitions = 10  # Times to run the algorithm
+rs = [10, 20, 50, 100, 200, 500, 1000]  # resources per parameter setting (reads/resource_factor = boots)
+frac_r_exploration = [0.05, 0.1, 0.2, 0.5, 0.75]
+R_budgets = [i*10**j for i in [1, 1.5, 2, 3, 5, 7] for j in [3, 4, 5]] + [1e6] # Total search budget (reads)
+experiments = rs * repetitions
+df_name_progress = df_name_results.rsplit('.')[0] + '_progress_binary.pkl'
+df_path_progress = os.path.join(results_path, df_name_progress)
+search_metric = 'median_'+response_column
+compute_metric = 'median_'+response_column
+df_search = df_stats_interpolated[
+    parameter_names + list(set([compute_metric] + [search_metric])) + ['boots','reads']
+].set_index(
+    parameter_names + ['boots']
+).sort_index()
+df_reads = df_stats_interpolated[
+    parameter_names + ['reads']
+].set_index(
+    parameter_names
+).sort_index()
+
+r_indices = []
+resource_proportional_parameters = ['sweep', 'replica']
+for r_parameters in resource_proportional_parameters:
+    if r_parameters in parameter_names:
+        r_indices.append(parameter_names.index(r_parameters))
+# Set the parameter sets as those in the results dataframe
+parameter_sets = list(set(df_results_interpolated['params'].values))
+use_raw_dataframes = False
+if os.path.exists(df_path_progress) and not use_raw_dataframes:
+    df_progress_total = pd.read_pickle(df_path_progress)
+else:
+    progress_list = []
+    for R_budget in R_budgets:
+        for frac_expl_total in frac_r_exploration:
+            # budget for exploration (runs)
+            R_exploration = int(R_budget*frac_expl_total)
+            # budget for exploitation (runs)
+            R_exploitation = R_budget - R_exploration
+            for r in rs:
+                if r > R_exploration:
+                    print(
+                        "R_exploration must be larger than single exploration step")
+                    continue
+                for experiment in range(repetitions):
+                    random_parameter_sets = random.choices(
+                        parameter_sets, k=int(R_exploration / r))
+                    # Conservative estimate of very unlikely scenario that we choose all sweeps=1
+                    # % Question: Should we replace these samples? We are replacing now
+
+                    
+                    all_reads = df_reads.loc[
+                        idx[random_parameter_sets[0]]
+                    ]
+                    resource_factor = 1
+                    for r_index in r_indices:
+                        resource_factor *= random_parameter_sets[0][r_index]
+                    actual_reads = take_closest(all_reads.values,r*resource_factor)[0]
+                    # Start random exploration with best found point
+                    series_list = [df_search.loc[idx[random_parameter_sets[0]+((actual_reads / resource_factor),)]]]
+                    total_reads = actual_reads
+
+                    if total_reads > R_exploration:
+                        # TODO: There should be a better way of having parameters that affect runtime making an appear. An idea, having a function f(params) = runs that we can call
+                        print(
+                            "R_exploration must be larger than single exploration step")
+                        continue
+                        # We allow it to run at least once assuming that
+                    for random_parameter_set in random_parameter_sets:
+                        resource_factor = 1
+                        for r_index in r_indices:
+                            resource_factor *= random_parameter_set[r_index]
+                        total_reads += r*resource_factor
+                        if total_reads > R_exploration:
+                            converged = True
+                            break
+                        series_list.append(
+                            df_search.loc[
+                                idx[random_parameter_set + (r,)]
+                            ]
+                        )
+                    exploration_step = pd.concat(series_list, axis=1).T.rename_axis(
+                        parameter_names + ['boots'])
+                    exploration_step[compute_metric] = exploration_step[compute_metric].expanding(
+                        min_periods=1).max()
+                    exploration_step.reset_index('boots', inplace=True)
+                    exploration_step['experiment'] = experiment
+                    exploration_step['run_per_solve'] = r
+                    exploration_step['R_explor'] = R_exploration
+                    exploration_step['R_exploit'] = R_exploitation
+                    exploration_step['R_budget'] = R_budget
+                    exploration_step['cum_reads'] = exploration_step.groupby('experiment').expanding(
+                        min_periods=1)['reads'].sum().reset_index(drop=True).values
+                    progress_list.append(exploration_step)
+                    # Exploitation step
+                    exploitation_step = df_search.reset_index().set_index(
+                        parameter_names).loc[exploration_step.nlargest(1, compute_metric).index]
+                    exploitation_step['cum_reads'] = exploitation_step['reads'] + \
+                        exploration_step['cum_reads'].max()
+                    exploitation_step.sort_values(['cum_reads'], inplace=True)
+                    exploitation_step = exploitation_step[exploitation_step['cum_reads'] <= R_budget]
+                    exploitation_step[compute_metric].fillna(
+                        0, inplace=True)
+                    exploitation_step[compute_metric].clip(
+                        lower=exploration_step[compute_metric].max(), inplace=True)
+                    exploitation_step[compute_metric] = exploitation_step[compute_metric].expanding(
+                        min_periods=1).max()
+                    exploitation_step['experiment'] = experiment
+                    exploitation_step['run_per_solve'] = r
+                    exploitation_step['R_explor'] = R_exploration
+                    exploitation_step['R_exploit'] = R_exploitation
+                    exploitation_step['R_budget'] = R_budget
+                    progress_list.append(exploitation_step)
+    df_progress_total = pd.concat(progress_list, axis=0)
+    df_progress_total.reset_index(inplace=True)
+    df_progress_total.to_pickle(df_path_progress)
+
+df_progress_total['f_explor'] = df_progress_total['R_explor'] / df_progress_total['R_budget']
+df_progress_total.to_pickle(df_path_progress)
+# for stat_measure in ['median']:
+#     if 'best_' + stat_measure + '_perf_ratio' not in df_progress_total.columns:
+#         df_progress_total[stat_measure + '_inv_perf_ratio'] = 1 - \
+#             df_progress_total[stat_measure + '_perf_ratio'] + EPSILON
+#         df_progress_total['best_' + stat_measure + '_inv_perf_ratio'] = df_progress_total.sort_values(
+#             ['cum_reads', 'R_budget']
+#         ).expanding(min_periods=1).min()[stat_measure + '_inv_perf_ratio']
+#         df_progress_total['best_' + stat_measure + '_perf_ratio'] = 1 - \
+#             df_progress_total['best_' + stat_measure +
+#                               '_inv_perf_ratio'] + EPSILON
+# df_progress_total = cleanup_df(df_progress_total)
+# df_progress_total.to_pickle(df_path)
+# %%
+df_progress_best, df_progress_end = process_df_progress(
+    df_progress=df_progress_total,
+    compute_metrics=['median_perf_ratio'],
+    stat_measures=['mean'],
+    maximizing=True,
+)
 # %%
 print(datetime.datetime.now())
 

2f2eb4ebd1b9afc44823e1be67cdbdf64b8335bc
bernalde <bernalde@cmu.edu>
2022-08-09 14:36:23 -0400

    Formatting and documenting some functions

--- src/names.py
+++ src/names.py
@@ -1,20 +1,21 @@
 import os
 
+
 class paths:
     def __init__(self, home_dir):
         self.cwd = home_dir
         self.raw_data = os.path.join(self.cwd, 'exp_raw')
         self.checkpoints = os.path.join(self.cwd, 'checkpoints')
         self.plots = os.path.join(self.cwd, 'plots')
-        
+
         for path in [self.checkpoints, self.plots]:
             if not os.path.exists(path):
                 os.makedirs(path)
-    
+
     def bootstrap(self):
         return os.path.join(self.checkpoints, 'bootstrap_results.pkl')
 
-            #     def param_path(self, base, param_dict, ignore = []):
+        #     def param_path(self, base, param_dict, ignore = []):
 #         if case == 'results':
 #             ppath = os.path.join(self.results, param2filename(param_dict, '', ignore))
 #         elif case == 'data':
@@ -24,30 +25,61 @@ class paths:
 #         elif case == 'instances':
 #             ppath = os.path.join(self.instances, param2filename(param_dict, '', ignore))
 #         elif case is None:
-#             ppath = os.path.join(self.cwd, param2filename(param_dict, '', ignore)) 
-        
-        
+#             ppath = os.path.join(self.cwd, param2filename(param_dict, '', ignore))
+
+
 #         if not os.path.exists(ppath):
 #             os.makedirs(ppath)
 #         return ppath
-                   
-        
+
+
 def param2filename(param_dict, ext, ignore=[]):
-    val2str = lambda v : str(v)  #update this for better float formatting
-    filename = ['{}={}_'.format(str(k), val2str(param_dict[k])) for\
-            k in sorted(param_dict.keys()) if\
-            k not in ignore]
+    """
+    Utility to turn parameter dictionary into filename
+
+    Parameters
+    ----------
+    param_dict : dict
+        dictionary of parameters
+    ext : str
+        extension of filename
+    ignore : list
+        list of parameters to ignore
+
+    Returns
+    -------
+    filename : str
+        filename
+    """
+    def val2str(v): return str(v)  # update this for better float formatting
+    filename = ['{}={}_'.format(str(k), val2str(param_dict[k])) for
+                k in sorted(param_dict.keys()) if
+                k not in ignore]
     filename = ''.join(filename)
     filename = filename[:-1] + ext
 
     return filename
 
+
 def filename2param(filename):
-    #utility to turn filename into parameter dictionary, user needs to fix types of values
+    """
+    Utility to turn filename into parameter dictionary, user needs to fix types of values
+
+    Parameters
+    ----------
+    filename : str
+        filename
+
+    Returns
+    -------
+    param_dict : dict
+        dictionary of parameters
+    """
+    # utility to turn filename into parameter dictionary, user needs to fix types of values
     split_filename = filename.split('_')
     ext_idx = split_filename[-1].rindex('.')
     split_filename[-1] = split_filename[-1][:ext_idx]
-    
+
     param_dict = {}
     for kv in split_filename:
         k, v = kv.split('=')
--- src/stochastic_benchmark.py
+++ src/stochastic_benchmark.py
@@ -5,28 +5,29 @@ import bootstrap
 import df_utils
 import names
 
+
 class stochastic_benchmark:
     def __init__(self, here=os.getcwd(), recover=True):
         # parameters should be user specified
-        
+
         # Initialize saving/filename utilities
         self.here = names.path(here)
-        
+
         # Check if checkpointed dataframes all exist -> if so, read all and exit
-        
-        
-        # Otherwise begin recovery process 
-        ## Read raw datafiles: expect that all files in exp_raw folder are pkls to go in
-        self.raw_data = df_utils.read_exp_raw(self.here) #If parameters need to be recovered from filenames, should be specified here
-        ## Don't save this step since relatively cheap
-        
-        ## Bootstrap
+
+        # Otherwise begin recovery process
+        # Read raw datafiles: expect that all files in exp_raw folder are pkls to go in
+        # If parameters need to be recovered from filenames, should be specified here
+        self.raw_data = df_utils.read_exp_raw(self.here)
+        # Don't save this step since relatively cheap
+
+        # Bootstrap
         if os.path.exists(self.here.bootstrap):
             self.bs_df = pd.read_pickle(self.here.bootstrap)
         else:
-            self.bs_df = bootstrap.Bootstrap(self.raw_data, group_on, bootstraps) #figure out how to store these parameters
+            # figure out how to store these parameters
+            self.bs_df = bootstrap.Bootstrap(
+                self.raw_data, group_on, bootstraps)
             self.bs_df.to_pick(self.here.bootstrap)
-        
-        ## Stats stuff
-        
-        
+
+        # Stats stuff

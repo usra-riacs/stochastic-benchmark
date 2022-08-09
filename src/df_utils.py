50a1304ee7d001865a5a220e1c2b26dc8445b561
bernalde <bernalde@cmu.edu>
2022-08-09 14:36:23 -0400

    Formatting and documenting some functions

--- src/df_utils.py
+++ src/df_utils.py
@@ -5,6 +5,7 @@ import names
 import numpy as np
 
 import parameters
+# TODO PyLance is creying here because parameters is not defined
 
 EPSILON = 1e-10
 confidence_level = 68
@@ -13,8 +14,22 @@ gap = 1.0
 
 
 def read_exp_raw(exp_raw_dir, name_params = []):
-    """ Input: directory of raw data files, parameter names that should be extracted from filename
-        Outputs: combined dataframe of files in raw data directory
+    """
+    Generates a combined dataframe of all experiments in exp_raw_dir
+
+    Parameters
+    ----------
+    exp_raw_dir : str
+        directory of raw data files, parameter names that should be extracted from filename
+    name_params : list
+        list of parameter names
+        
+        
+    Returns
+    -------
+    df : pd.dataframe
+        dataframe of all experiments in exp_raw_dir
+        Directory containing raw experiment data
     """
     filelist = glob.glob(os.path.join(exp_raw_dir, '*.pkl'))
     df_list = []
@@ -29,8 +44,20 @@ def read_exp_raw(exp_raw_dir, name_params = []):
     return df_all
 
 def parameter_set(df, param_names):
-    """ Input: dataframe and parameters of interest
-        Outputs: list of numerical values for parameters encountered in df
+    """
+    Obtain the list of parameter settings in columns param_names from a dataframe and create 'params' column in it
+
+    Parameters
+    ----------
+    df : pd.dataframe
+        dataframe containing parameter settings
+    param_names : list
+        list of parameter names
+
+    Returns
+    -------
+    param_set : list
+        list of unique parameter settings
     """
     df['params'] = df[param_names].apply(tuple, axis=1)
     param_set = df['params'].unique()
@@ -38,8 +65,23 @@ def parameter_set(df, param_names):
 
 def get_best(df, response_col, response_dir, group_on):
     """ 
-    Outputs: dataframe rows with best response (specificied by response_col and response_dir)
-    Grouped by group on
+    Sorts according to the best response for each parameter set in dataframe fd (specificied by response_col and response_dir) grouped by group on
+
+    Parameters
+    ----------
+    df : pd.dataframe
+        dataframe containing parameter settings
+    response_col : str
+        column name of response
+    response_dir : float
+        direction of response (either 'min'=-1 or 'max'=1)
+        TODO There has to be a better way of doing this
+    group_on : list
+        list of column names to group on
+
+    Returns
+    -------
+    df_best : pd.dataframe
     """
     if response_dir == -1: # Minimization
         best_df = df.sort_values(response_col, ascending=True).drop_duplicates(group_on)
